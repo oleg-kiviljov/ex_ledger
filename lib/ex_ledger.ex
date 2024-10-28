77782dc7d6a10d63e4da8acd1a0cfaa6adcab21c
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2024-10-28 14:43:11 +0200

    Initial commit

---
+++ lib/ex_ledger.ex
@@ -0,0 +1,18 @@
+defmodule ExLedger do
+  @moduledoc """
+  Documentation for `ExLedger`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExLedger.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
