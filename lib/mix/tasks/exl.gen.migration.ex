e3db9e352b21a0ec6a7f8e7bc3ca53b39251d6b2
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2024-10-28 16:57:06 +0200

    WIP

--- lib/mix/tasks/exl.gen.migration.ex
+++ lib/mix/tasks/exl.gen.migration.ex
@@ -4,33 +4,28 @@ defmodule Mix.Tasks.Exl.Gen.Migration do
   @migration_dir "priv/repo/migrations"
 
   def run(_args) do
-    Mix.Project.config() |> IO.inspect()
-
-    # generate_migration_file("create_enums")
+    generate_migration_file("create_enums")
     # generate_migration_file("create_accounts")
     # generate_migration_file("create_transactions")
   end
 
-  # defp parse_args(args) do
-  #   case args do
-  #     [migration_name | content] ->
-  #       {migration_name, Enum.join(content, " ")}
-  #     _ ->
-  #       Mix.raise("Usage: mix generate_migration <migration_name> <content>")
-  #   end
-  # end
+  defp host_app do
+    Mix.Project.config().app
+  end
 
-  # defp generate_migration_file(migration_name, content) do
-  #   timestamp = timestamp()
-  #   migration_file_name = "#{@migration_dir}/#{timestamp}_#{migration_name}.exs"
+  defp generate_migration_file(migration_name) do
+    Mix.Task.run("ecto.gen.migration", [migration_name])
 
-  #   # Ensure the migrations directory exists
-  #   File.mkdir_p!(@migration_dir)
+    # timestamp = timestamp()
+    # migration_file_name = "#{@migration_dir}/#{timestamp}_#{migration_name}.exs"
 
-  #   # Create the migration file with the specified content
-  #   File.write!(migration_file_name, generate_migration_content(content))
-  #   Mix.shell().info("Migration file created: #{migration_file_name}")
-  # end
+    # # Ensure the migrations directory exists
+    # File.mkdir_p!(@migration_dir)
+
+    # # Create the migration file with the specified content
+    # File.write!(migration_file_name, generate_migration_content(content))
+    # Mix.shell().info("Migration file created: #{migration_file_name}")
+  end
 
   # defp timestamp do
   #   DateTime.utc_now()
