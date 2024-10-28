20d5c01f01919234fc8c1a7ad0b54f00e6e2c8fd
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2024-10-29 00:10:04 +0200

    Add public api

--- lib/ex_ledger.ex
+++ lib/ex_ledger.ex
@@ -1,18 +1,63 @@
 defmodule ExLedger do
   @moduledoc """
-  Documentation for `ExLedger`.
+  Public API.
   """
 
-  @doc """
-  Hello world.
+  alias ExLedger.Accounts.{
+    Account,
+    CreateAccount,
+    UpdateAccountProperties,
+    UpdateAccountStatus
+  }
 
-  ## Examples
+  alias ExLedger.Transactions.{
+    ConfirmDeposit,
+    ConfirmWithdrawal,
+    CreateDeposit,
+    CreateWithdrawal,
+    FailDeposit,
+    FailWithdrawal,
+    Transaction,
+    UpdateTransactionProperties
+  }
 
-      iex> ExLedger.hello()
-      :world
+  @spec create_account(CreateAccount.params()) ::
+          {:ok, Account.t()} | {:error, Ecto.Changeset.t()}
+  def create_account(params), do: CreateAccount.execute(params)
 
-  """
-  def hello do
-    :world
-  end
+  @spec update_account_status(UpdateAccountStatus.params()) ::
+          {:ok, Account.t()} | {:error, Ecto.Changeset.t()}
+  def update_account_status(params), do: UpdateAccountStatus.execute(params)
+
+  @spec update_account_properties(UpdateAccountProperties.params()) ::
+          {:ok, Account.t()} | {:error, :transaction_not_found | Ecto.Changeset.t()}
+  def update_account_properties(params), do: UpdateAccountProperties.execute(params)
+
+  @spec create_deposit(CreateDeposit.params()) ::
+          {:ok, Transaction.t()} | {:error, Ecto.Changeset.t()}
+  def create_deposit(params), do: CreateDeposit.execute(params)
+
+  @spec confirm_deposit(ConfirmDeposit.params()) ::
+          {:ok, Transaction.t()} | {:error, Ecto.Changeset.t()}
+  def confirm_deposit(params), do: ConfirmDeposit.execute(params)
+
+  @spec fail_deposit(FailDeposit.params()) ::
+          {:ok, Transaction.t()} | {:error, Ecto.Changeset.t()}
+  def fail_deposit(params), do: FailDeposit.execute(params)
+
+  @spec create_withdrawal(CreateWithdrawal.params()) ::
+          {:ok, Transaction.t()} | {:error, Ecto.Changeset.t()}
+  def create_withdrawal(params), do: CreateWithdrawal.execute(params)
+
+  @spec confirm_withdrawal(ConfirmWithdrawal.params()) ::
+          {:ok, Transaction.t()} | {:error, Ecto.Changeset.t()}
+  def confirm_withdrawal(params), do: ConfirmWithdrawal.execute(params)
+
+  @spec fail_withdrawal(FailWithdrawal.params()) ::
+          {:ok, Transaction.t()} | {:error, Ecto.Changeset.t()}
+  def fail_withdrawal(params), do: FailWithdrawal.execute(params)
+
+  @spec update_transaction_properties(UpdateTransactionProperties.params()) ::
+          {:ok, Transaction.t()} | {:error, :transaction_not_found | Ecto.Changeset.t()}
+  def update_transaction_properties(params), do: UpdateTransactionProperties.execute(params)
 end
