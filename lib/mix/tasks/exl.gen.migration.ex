99324a18337e5e13383834bea5b89a913a21af92
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2024-10-29 14:12:59 +0200

    Compact migrations into single one

--- lib/mix/tasks/exl.gen.migration.ex
+++ lib/mix/tasks/exl.gen.migration.ex
@@ -1,12 +1,10 @@
-defmodule Mix.Tasks.Exl.Gen.Migrations do
+defmodule Mix.Tasks.Exl.Gen.Migration do
   use Mix.Task
 
   @repo Application.compile_env!(:ex_ledger, :repo)
 
   def run(_args) do
-    generate_migration("create_enums")
-    generate_migration("create_accounts")
-    generate_migration("create_transactions")
+    generate_migration("add_ex_ledger")
   end
 
   defp generate_migration(template_name) do
@@ -16,7 +14,7 @@ defmodule Mix.Tasks.Exl.Gen.Migrations do
     migration_content =
       :ex_ledger
       |> :code.priv_dir()
-      |> Path.join("templates/exl.gen.migrations/#{template_name}.ex")
+      |> Path.join("templates/exl.gen.migration/#{template_name}.ex")
       |> File.read!()
       |> EEx.eval_string(repo: @repo)
 
@@ -24,8 +22,6 @@ defmodule Mix.Tasks.Exl.Gen.Migrations do
     File.write!(migration_name, migration_content)
 
     Mix.shell().info(["\e[32m* creating\e[0m ", migration_name])
-
-    Process.sleep(1000)
   end
 
   defp to_snakecase(module_name) do
---
+++ priv/templates/exl.gen.migration/add_ex_ledger.ex
@@ -0,0 +1,55 @@
+defmodule <%= inspect repo %>.Migrations.AddExLedger do
+  use Ecto.Migration
+
+  alias ExLedger.Enums.{
+    AccountStatus,
+    AccountType,
+    Currency,
+    TransactionStatus,
+    TransactionType
+  }
+
+  def change do
+    AccountStatus.create_type()
+    AccountType.create_type()
+    Currency.create_type()
+    TransactionStatus.create_type()
+    TransactionType.create_type()
+
+    create table(:accounts) do
+      add(:balance, :decimal, precision: 28, scale: 12, null: false)
+      add(:currency, Currency.type(), null: false)
+      add(:type, AccountType.type(), null: false)
+      add(:status, AccountStatus.type(), null: false)
+      add(:properties, :map)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create(index(:accounts, [:balance]))
+    create(index(:accounts, [:currency]))
+    create(index(:accounts, [:type]))
+    create(index(:accounts, [:status]))
+    create(index(:accounts, [:properties], using: :gin))
+
+    create(
+      constraint(:accounts, :balance_must_be_greater_than_or_equal_to_zero, check: "balance >= 0")
+    )
+
+    create table(:transactions) do
+      add(:amount, :decimal, precision: 28, scale: 12, null: false)
+      add(:type, TransactionType.type(), null: false)
+      add(:status, TransactionStatus.type(), null: false)
+      add(:properties, :map)
+      add(:account_id, references(:accounts), null: false)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create(index(:transactions, [:type]))
+    create(index(:transactions, [:status]))
+    create(index(:transactions, [:account_id]))
+    create(index(:transactions, [:properties], using: :gin))
+    create(constraint(:transactions, :amount_must_be_greater_than_zero, check: "amount > 0"))
+  end
+end
