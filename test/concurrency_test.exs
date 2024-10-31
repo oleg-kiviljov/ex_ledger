5ad518a2b6d7e2321321c598b3871d76f18330a9
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2024-10-31 14:49:20 +0200

    WIP

--- test/concurrency_test.exs
+++ test/concurrency_test.exs
@@ -3,17 +3,41 @@ defmodule ExLedger.ConcurrencyTest do
   @moduletag :no_sandbox
 
   describe "confirm_deposit/1" do
-    test "ensures database locks are handled correctly" do
-      account = create_account!()
+    setup do
+      %{account: create_account!()}
+    end
 
+    test "ensures database locks are handled correctly for single deposit", %{account: account} do
+      %Transaction{id: transaction_id} =
+        deposit = create_deposit!(account, Decimal.new(:rand.uniform(100)))
+
+      results =
+        Enum.map(1..10, fn _ ->
+          Task.async(fn ->
+            unboxed_run(fn ->
+              ExLedger.confirm_deposit(%{transaction_id: deposit.id})
+            end)
+          end)
+        end)
+        |> Task.await_many()
+
+      assert {:ok, %Transaction{id: ^transaction_id, status: :confirmed}} =
+               Keyword.fetch(results, :ok)
+
+      results
+      |> Keyword.drop([:ok])
+      |> Enum.all?(fn {:error, reason} ->
+        assert reason == :transaction_already_processed
+      end)
+
+      assert %Account{balance: account_balance} = Repo.get(Account, account.id)
+      assert Decimal.eq?(account_balance, deposit.amount)
+    end
+
+    test "ensures database locks are handled correctly for multiple deposits", %{account: account} do
       deposits =
         Enum.map(1..10, fn _ ->
           create_deposit!(account, Decimal.new(:rand.uniform(100)))
-        end)
-
-      total_deposited_amount =
-        Enum.reduce(deposits, Decimal.new(0), fn deposit, acc ->
-          Decimal.add(acc, deposit.amount)
         end)
 
       deposits
@@ -27,20 +51,60 @@ defmodule ExLedger.ConcurrencyTest do
       end)
       |> Task.await_many()
 
+      total_deposited_amount =
+        Enum.reduce(deposits, Decimal.new(0), fn deposit, acc ->
+          Decimal.add(acc, deposit.amount)
+        end)
+
       assert %Account{balance: account_balance} = Repo.get(Account, account.id)
       assert Decimal.eq?(account_balance, total_deposited_amount)
     end
   end
 
-  describe "create_withdrawal/1" do
-    test "ensures database locks are handled correctly" do
-      starting_account_balance = Decimal.new(10_000)
+  describe "fail_deposit/1" do
+    test "ensures database locks are handled correctly for single deposit" do
       account = create_account!()
 
+      %Transaction{id: transaction_id} =
+        deposit = create_deposit!(account, Decimal.new(:rand.uniform(100)))
+
+      results =
+        Enum.map(1..10, fn _ ->
+          Task.async(fn ->
+            unboxed_run(fn ->
+              ExLedger.fail_deposit(%{transaction_id: deposit.id})
+            end)
+          end)
+        end)
+        |> Task.await_many()
+
+      assert {:ok, %Transaction{id: ^transaction_id, status: :failed}} =
+               Keyword.fetch(results, :ok)
+
+      results
+      |> Keyword.drop([:ok])
+      |> Enum.all?(fn {:error, reason} ->
+        assert reason == :transaction_already_processed
+      end)
+    end
+  end
+
+  describe "create_withdrawal/1" do
+    setup do
+      account = create_account!()
+      initial_account_balance = Decimal.new(10_000)
+
       account
-      |> create_deposit!(starting_account_balance)
+      |> create_deposit!(initial_account_balance)
       |> confirm_deposit!()
 
+      %{account: account, initial_account_balance: initial_account_balance}
+    end
+
+    test "ensures database locks are handled correctly for multiple withdrawals", %{
+      account: account,
+      initial_account_balance: initial_account_balance
+    } do
       withdrawals =
         Enum.map(1..10, fn _ ->
           Task.async(fn ->
@@ -63,9 +127,115 @@ defmodule ExLedger.ConcurrencyTest do
         end)
 
       assert %Account{balance: account_balance} = Repo.get(Account, account.id)
-      expected_account_balance = Decimal.sub(starting_account_balance, total_withdrawn_amount)
+      expected_account_balance = Decimal.sub(initial_account_balance, total_withdrawn_amount)
 
       assert Decimal.eq?(account_balance, expected_account_balance)
+    end
+  end
+
+  describe "confirm_withdrawal/1" do
+    setup do
+      account = create_account!()
+      initial_account_balance = Decimal.new(10_000)
+
+      account
+      |> create_deposit!(initial_account_balance)
+      |> confirm_deposit!()
+
+      %{account: account}
+    end
+
+    test "ensures database locks are handled correctly for single withdrawal", %{account: account} do
+      %Transaction{id: transaction_id} =
+        withdrawal = create_withdrawal!(account, Decimal.new(:rand.uniform(100)))
+
+      results =
+        Enum.map(1..10, fn _ ->
+          Task.async(fn ->
+            unboxed_run(fn ->
+              ExLedger.confirm_withdrawal(%{transaction_id: withdrawal.id})
+            end)
+          end)
+        end)
+        |> Task.await_many()
+
+      assert {:ok, %Transaction{id: ^transaction_id, status: :confirmed}} =
+               Keyword.fetch(results, :ok)
+
+      results
+      |> Keyword.drop([:ok])
+      |> Enum.all?(fn {:error, reason} ->
+        assert reason == :transaction_already_processed
+      end)
+    end
+  end
+
+  describe "fail_withdrawal/1" do
+    setup do
+      account = create_account!()
+      initial_account_balance = Decimal.new(10_000)
+
+      account
+      |> create_deposit!(initial_account_balance)
+      |> confirm_deposit!()
+
+      %{account: account, initial_account_balance: initial_account_balance}
+    end
+
+    test "ensures database locks are handled correctly for single withdrawal", %{
+      account: account,
+      initial_account_balance: initial_account_balance
+    } do
+      %Transaction{id: transaction_id} =
+        withdrawal = create_withdrawal!(account, Decimal.new(:rand.uniform(100)))
+
+      results =
+        Enum.map(1..10, fn _ ->
+          Task.async(fn ->
+            unboxed_run(fn ->
+              ExLedger.fail_withdrawal(%{transaction_id: withdrawal.id})
+            end)
+          end)
+        end)
+        |> Task.await_many()
+
+      assert {:ok, %Transaction{id: ^transaction_id, status: :failed}} =
+               Keyword.fetch(results, :ok)
+
+      results
+      |> Keyword.drop([:ok])
+      |> Enum.all?(fn {:error, reason} ->
+        assert reason == :transaction_already_processed
+      end)
+
+      assert %Account{balance: account_balance} = Repo.get(Account, account.id)
+      assert Decimal.eq?(account_balance, initial_account_balance)
+    end
+
+    test "ensures database locks are handled correctly for multiple withdrawals", %{
+      account: account,
+      initial_account_balance: initial_account_balance
+    } do
+      withdrawals =
+        Enum.map(1..10, fn _ ->
+          create_withdrawal!(account, Decimal.new(:rand.uniform(100)))
+        end)
+
+      withdrawals
+      |> Enum.shuffle()
+      |> Enum.map(fn withdrawal ->
+        Task.async(fn ->
+          unboxed_run(fn ->
+            ExLedger.fail_withdrawal(%{
+              transaction_id: withdrawal.id
+            })
+          end)
+        end)
+      end)
+      |> Task.await_many()
+
+      assert %Account{balance: account_balance} = Repo.get(Account, account.id)
+      assert Decimal.eq?(account_balance, initial_account_balance)
     end
   end
 end
