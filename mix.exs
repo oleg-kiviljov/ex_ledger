8d7e9dc1295e57bc4fcffea98d5dc0b0a670dc81
Oleg Kiviljov <oleg.kiviljov@gmail.com>
2024-11-01 03:00:47 +0200

    Downgrade elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule ExLedger.MixProject do
     [
       app: :ex_ledger,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
